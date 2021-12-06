58fc30b768cc6c5eddce3f0b34b583383a1c830d
lucas vinicius <lucasviniciusaalves@gmail.com>
2021-12-06 11:35:10 -0300

    add: S.O.L.I.D

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -8,13 +9,8 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (Request, Response) => {
   const { name, description } = Request.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-  if (categoryAlreadyExists) {
-    return Response.status(400).json({ error: 'Category already exists' });
-  }
-
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
 
   Response.status(201).send();
 });

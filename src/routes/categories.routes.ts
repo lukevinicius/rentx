1d9380af4b5081fac329b97df545a342b508ae42
lucas vinicius <lucasviniciusaalves@gmail.com>
2021-12-06 10:36:41 -0300

    add: Category already exists

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (Request, Response) => {
   const { name, description } = Request.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return Response.status(400).json({ error: 'Category already exists' });
+  }
+
   categoriesRepository.create({ name, description });
 
   Response.status(201).send();

1f793329dd5753088fdc51963dd149a394158b84
lucas vinicius <lucasviniciusaalves@gmail.com>
2021-12-06 14:55:54 -0300

    add: useCase Category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,13 @@
 import { Router } from 'express';
 
 import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (Request, Response) => {
-  const { name, description } = Request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-  createCategoryService.execute({ name, description });
-
-  Response.status(201).send();
+  return createCategoryController.handle(Request, Response);
 });
 
 categoriesRoutes.get('/', (Request, Response) => {

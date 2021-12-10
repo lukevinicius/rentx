94b0ef2952cf3ac5a3d2ac7d8462231ebd1c782a
lucas vinicius <lucasviniciusaalves@gmail.com>
2021-12-10 15:08:13 -0300

    add: swagger

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import log from './config/logger';
 import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,6 +11,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 

509aecc5eeb5f6d0d5fef3c6cd9ff3ba8095815e
lucas vinicius <lucasviniciusaalves@gmail.com>
2022-01-21 09:49:08 -0300

    add: middleware ensure admin

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -2,6 +2,6 @@ import { ICreateCarDTO } from '../dtos/ICreateCarDTO';
 import { Car } from '../infra/typeorm/entities/Car';
 
 export interface ICarsRepository {
-  create(data: ICreateCarDTO): Promise<void>;
+  create(data: ICreateCarDTO): Promise<Car>;
   findByLicencePlate(license_plate: string): Promise<Car>;
 }
